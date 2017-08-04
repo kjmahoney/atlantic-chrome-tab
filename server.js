3e1af2835e4d4357c5a1c9f9381670dabb232544
Andrew McGill <arm5077@gmail.com>
2017-08-04 16:26:23 -0400

    Add 2 hour caching

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ require('dotenv').config()
 var sitePath = process.argv[2] || ".";
 var port = process.env.PORT || 5000;
 var request = require('request');
+var apicache = require('apicache');
 
 var express = require('express');
 var app = express();
+var cache = apicache.middleware;
 
 app.use((req, res, next) => {
   console.log(req.url);
   next();
 });
 
-app.get('/api', (req, res) => {
+app.get('/api', cache('2 hours'), (req, res) => {
   request({
     url: 'http://www.theatlantic.com/api/2.0/articles/',
     headers: {
