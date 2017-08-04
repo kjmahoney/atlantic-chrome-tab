5930eb6808852b58de5a40c323b110983aa465d8
Kevin Mahoney <kmahoney@theatlantic.com>
2017-08-04 15:57:40 -0400

    fix CORS error

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-let request = new Request("http://localhost:5000/api");
+let request = new Request("https://atlantic-new-tab.herokuapp.com/api/");
 
-console.log(request);
+// let request = ['a', 'b', 'c']
 
 fetch(request).then(function(response) {
-  return response;
-}).then(function(json){
-  console.log("JSON" + json);
+    return response.json().then(function(json){
+      console.dir(json.results[0]);
+      let dekText = json.results[0].dek;
+      let titleText = json.results[0].title;
+      setDek(dekText, titleText);
+  });
 });
+
+document.getElementsByClassName("c-tab__dek")[0].textContent = 'kevin';
+
+const setDek = (dekText, titleText) => {
+  document.getElementById("title").textContent = titleText;
+  document.getElementById("dek").textContent = dekText;
+}
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
 var sitePath = process.argv[2] || ".";
 var port = process.env.PORT || 5000;
 var request = require('request');
+var cors = require('cors')
 
 var express = require('express');
 var app = express();
+
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.url);
@@ -11,7 +15,12 @@ app.use((req, res, next) => {
 });
 
 app.get('/api', (req, res) => {
-  request('http://localhost:9001/api/2.0/articles/', (error, response, body) => {
+  request({
+    url: 'http://www.theatlantic.com/api/2.0/articles/',
+    headers: {
+      'Authorization': 'token ' + process.env.ATLANTIC_API_TOKEN
+    }
+  }, (error, response, body) => {
     body = JSON.parse(body)
     res.json(body);
   })
