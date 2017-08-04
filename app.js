03721fdc359556e483ab1970d45d95c2ef820c28
Kevin Mahoney <kmahoney@theatlantic.com>
2017-08-04 17:07:40 -0400

    --amend

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 let request = new Request("https://atlantic-new-tab.herokuapp.com/api/");
 
-// let request = ['a', 'b', 'c']
-
 fetch(request).then(function(response) {
     return response.json().then(function(json){
-      console.dir(json.results[0]);
-      let dekText = json.results[0].dek;
-      let titleText = json.results[0].title;
-      setDek(dekText, titleText);
+
+      for (i=0; i<3; i++) {
+        document.getElementsByClassName("c-tab__dek")[i].innerHTML = json.results[i].dek;
+        document.getElementsByClassName("c-tab__title")[i].innerHTML = json.results[i].title;
+      }
   });
 });
-
-document.getElementsByClassName("c-tab__dek")[0].textContent = 'kevin';
-
-const setDek = (dekText, titleText) => {
-  document.getElementById("title").textContent = titleText;
-  document.getElementById("dek").textContent = dekText;
-}
